a6037b1df6f42f390f606dbd783b097777eedcb7
Aniket Thakur <304387636+aniketthakur9@users.noreply.github.com>
2026-08-07 15:03:08 +0530

    Updated Portfolio
    t

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on https://aniketportfolio.duckdns.org:${PORT}`);
 
 });
